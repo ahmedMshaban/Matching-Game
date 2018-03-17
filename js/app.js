dbaf58ec9488d641f6681138fa54b17920c6ae12
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-03-17 14:03:34 +0300

    add roundMoved, gameMoves

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-function flip() {
-    $('.playground-cell').toggleClass('flipped');
+let roundMoves = 0;
+let gameMove = 0;
+
+function clickedMe(evt) {
+  roundMoves ++;
+  gameMove ++;
+  console.log(gameMove);
+  if(roundMoves > 2) {
+    roundMoves = 0;
+    $(".main-playground-container .playground-cell").removeClass("clicked");
+    $(".main-playground-container .playground-cell").removeClass("flipped");
+  }
+
+  $(evt.target).parent('.playground-cell').addClass('clicked');
 }
+
+function flip(evt) {
+  $(evt.target).parent('.playground-cell').toggleClass('flipped');
+}
+
+
+document.querySelector('.main-playground-container').addEventListener('click', function (evt) {
+
+     if($(evt.target).attr('class') === 'playground-cell-front') {
+      clickedMe(evt);
+      flip(evt);
+    }
+
+});
