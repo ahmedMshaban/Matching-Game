ca9231fd2f8a669cdb564970220303a78b94c520
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-03-18 14:53:15 +0300

    Add new images symobls folder

--- js/app.js
+++ js/app.js
@@ -1,29 +1,70 @@
 let roundMoves = 0;
 let gameMove = 0;
+let symbols = ['images/boat.png','images/boat.png','images/building.png','images/building.png','images/daruma.png','images/daruma.png','images/doll.png','images/doll.png','images/star.png','images/star.png','images/teddy-bear.png','images/teddy-bear.png','images/toys.png','images/toys.png','images/tricycle.png','images/tricycle.png']
 
-function clickedMe(evt) {
-  roundMoves ++;
-  gameMove ++;
-  console.log(gameMove);
-  if(roundMoves > 2) {
-    roundMoves = 0;
-    $(".main-playground-container .playground-cell").removeClass("clicked");
-    $(".main-playground-container .playground-cell").removeClass("flipped");
+function clickedCell(evt) {
+  if(roundMoves === 0) {
+    roundMoves ++;
+    $(evt.target).parent('.playground-cell').addClass('clicked');
+    $(evt.target).parent('.playground-cell').toggleClass('flipped');
   }
-
-  $(evt.target).parent('.playground-cell').addClass('clicked');
+  else if(roundMoves === 1) {
+    //Prevent more than 2 click on the same round
+    roundMoves = 2;
+    $(evt.target).parent('.playground-cell').toggleClass('flipped');
+    $(evt.target).parent('.playground-cell').addClass('clicked');
+    window.setTimeout(roundCheck, 1500);
+  }
 }
 
-function flip(evt) {
-  $(evt.target).parent('.playground-cell').toggleClass('flipped');
+function roundCheck() {
+  $(".main-playground-container .playground-cell").removeClass("flipped");
+  //Reset the round
+  roundMoves = 0;
+  gameMove ++;
 }
 
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
-document.querySelector('.main-playground-container').addEventListener('click', function (evt) {
-
-     if($(evt.target).attr('class') === 'playground-cell-front') {
-      clickedMe(evt);
-      flip(evt);
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
+    return array;
+}
+
+function distributionSymbols(array) {
+  shuffle(symbols);
+  for (let i = 1; i <= array.length; i++){
+    console.log(array[i]);
+    $('.playground-cell:nth-child('+ i+') img').attr('src',array[i]);
+  }
+}
+
+document.querySelector('.main-playground-container').addEventListener('click', function (evt) {
+     if($(evt.target).attr('class') === 'playground-cell-front' && roundMoves !== 2) {
+      clickedCell(evt);
+    }
 });
+
+
+
+
+console.log(distributionSymbols(symbols));
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
