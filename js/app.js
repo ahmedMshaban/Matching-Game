d839d2e8a0c437f4df5be4a6bf12aecb628d8e64
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-03-18 18:21:26 +0300

    Add identical functionality

--- js/app.js
+++ js/app.js
@@ -1,27 +1,44 @@
 let roundMoves = 0;
-let gameMove = 0;
-let symbols = ['images/boat.png','images/boat.png','images/building.png','images/building.png','images/daruma.png','images/daruma.png','images/doll.png','images/doll.png','images/star.png','images/star.png','images/teddy-bear.png','images/teddy-bear.png','images/toys.png','images/toys.png','images/tricycle.png','images/tricycle.png']
+let gameMoves = 0;
+let gameEnd = 0;
+const symbols = ['images/boat.png','images/boat.png','images/building.png','images/building.png','images/daruma.png','images/daruma.png','images/doll.png','images/doll.png','images/star.png','images/star.png','images/teddy-bear.png','images/teddy-bear.png','images/toys.png','images/toys.png','images/tricycle.png','images/tricycle.png']
+let openCell = [];
 
 function clickedCell(evt) {
   if(roundMoves === 0) {
     roundMoves ++;
     $(evt.target).parent('.playground-cell').addClass('clicked');
     $(evt.target).parent('.playground-cell').toggleClass('flipped');
+    openCell[0] = $(evt.target).siblings().children('img').attr('src');
+    console.log(openCell);
   }
   else if(roundMoves === 1) {
     //Prevent more than 2 click on the same round
     roundMoves = 2;
-    $(evt.target).parent('.playground-cell').toggleClass('flipped');
     $(evt.target).parent('.playground-cell').addClass('clicked');
+    $(evt.target).parent('.playground-cell').toggleClass('flipped');
+    openCell[1] = $(evt.target).siblings().children('img').attr('src');
+    console.log(openCell);
     window.setTimeout(roundCheck, 1500);
   }
 }
 
 function roundCheck() {
+  if(openCell[0] === openCell[1]) {
+    $( ".clicked" ).addClass('identical');
+    gameEnd ++;
+    if (gameEnd === 8) {
+      gameResult();
+    }
+  }
   $(".main-playground-container .playground-cell").removeClass("flipped");
+  $(".main-playground-container .playground-cell").removeClass("clicked");
   //Reset the round
   roundMoves = 0;
-  gameMove ++;
+  //Empty the aray
+  openCell = [];
+  gameMoves ++;
+
 }
 
 function shuffle(array) {
@@ -45,13 +62,17 @@ function distributionSymbols(array) {
   }
 }
 
+function gameResult() {
+  alert('Congratus your Win!!' );
+  alert(gameMoves);
+}
+
+
 document.querySelector('.main-playground-container').addEventListener('click', function (evt) {
      if($(evt.target).attr('class') === 'playground-cell-front' && roundMoves !== 2) {
       clickedCell(evt);
     }
 });
-
-
 
 
 console.log(distributionSymbols(symbols));
