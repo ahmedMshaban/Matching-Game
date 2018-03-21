89c7d2a18180670d74f832d572f1fae45b505ac4
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-03-21 20:09:28 +0300

    Add restart function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ let gameMoves = 0;
 let gameEndCount = 0;
 const symbols = ['images/boat.png','images/boat.png','images/building.png','images/building.png','images/daruma.png','images/daruma.png','images/doll.png','images/doll.png','images/star.png','images/star.png','images/teddy-bear.png','images/teddy-bear.png','images/toys.png','images/toys.png','images/tricycle.png','images/tricycle.png']
 let openCell = [];
+let timeExcuation;
+
 
 function clickedCell(evt) {
   if(gameMoves === 0 && roundMoves === 0) {
@@ -21,6 +23,9 @@ function clickedCell(evt) {
     $(evt.target).parent('.playground-cell').addClass('clicked');
     $(evt.target).parent('.playground-cell').toggleClass('flipped');
     openCell[1] = $(evt.target).siblings().children('img').attr('src');
+    gameMoves ++;
+    //Update game moves
+    $(".game-moves").text(gameMoves);
     window.setTimeout(roundCheck, 1500);
   }
 }
@@ -39,9 +44,6 @@ function roundCheck() {
   roundMoves = 0;
   //Empty the aray
   openCell = [];
-  gameMoves ++;
-  //Update game moves
-  $(".game-moves").text(gameMoves);
 }
 
 function shuffle(array) {
@@ -68,7 +70,7 @@ function distributionSymbols(array) {
 function timeCalculator() {
   let sec = 0;
   function pad ( val ) { return val > 9 ? val : "0" + val; }
-  setInterval( function(){
+  timeExcuation = setInterval( function(){
       if (gameEndCount === 8) {
         return;
       }
@@ -88,7 +90,17 @@ function gameResult() {
 }
 
 document.querySelector('.restart-game').addEventListener('click', function (evt) {
-
+  gameMoves = 0;
+  $(".game-moves").text(gameMoves);
+  gameEndCount = 0;
+  openCell = [];
+  $(".main-playground-container .playground-cell").removeClass("flipped");
+  $(".main-playground-container .playground-cell").removeClass("clicked");
+  $(".main-playground-container .playground-cell").removeClass("identical");
+  clearInterval(timeExcuation);
+  $("#seconds").text("00");
+  $("#minutes").text("00");
+  window.setTimeout(distributionSymbols, 500,symbols);
 });
 
 
@@ -98,5 +110,4 @@ document.querySelector('.main-playground-container').addEventListener('click', f
     }
 });
 
-
-console.log(distributionSymbols(symbols));
+distributionSymbols(symbols);
