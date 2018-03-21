739e162f48d5cb2bb08d343069d020f8429e3cec
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-03-21 19:08:29 +0300

    Add timeer functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 let roundMoves = 0;
 let gameMoves = 0;
-let gameEnd = 0;
+let gameEndCount = 0;
 const symbols = ['images/boat.png','images/boat.png','images/building.png','images/building.png','images/daruma.png','images/daruma.png','images/doll.png','images/doll.png','images/star.png','images/star.png','images/teddy-bear.png','images/teddy-bear.png','images/toys.png','images/toys.png','images/tricycle.png','images/tricycle.png']
 let openCell = [];
 
 function clickedCell(evt) {
+  if(gameMoves === 0 && roundMoves === 0) {
+    timeCalculator();
+  }
+
   if(roundMoves === 0) {
     roundMoves ++;
     $(evt.target).parent('.playground-cell').addClass('clicked');
     $(evt.target).parent('.playground-cell').toggleClass('flipped');
     openCell[0] = $(evt.target).siblings().children('img').attr('src');
-    console.log(openCell);
   }
   else if(roundMoves === 1) {
     //Prevent more than 2 click on the same round
@@ -18,7 +21,6 @@ function clickedCell(evt) {
     $(evt.target).parent('.playground-cell').addClass('clicked');
     $(evt.target).parent('.playground-cell').toggleClass('flipped');
     openCell[1] = $(evt.target).siblings().children('img').attr('src');
-    console.log(openCell);
     window.setTimeout(roundCheck, 1500);
   }
 }
@@ -26,9 +28,9 @@ function clickedCell(evt) {
 function roundCheck() {
   if(openCell[0] === openCell[1]) {
     $( ".clicked" ).addClass('identical');
-    gameEnd ++;
-    if (gameEnd === 8) {
-      gameResult();
+    gameEndCount ++;
+    if (gameEndCount === 8) {
+      gameEnd();
     }
   }
   $(".main-playground-container .playground-cell").removeClass("flipped");
@@ -38,7 +40,8 @@ function roundCheck() {
   //Empty the aray
   openCell = [];
   gameMoves ++;
-
+  //Update game moves
+  $(".game-moves").text(gameMoves);
 }
 
 function shuffle(array) {
@@ -62,15 +65,36 @@ function distributionSymbols(array) {
   }
 }
 
+function timeCalculator() {
+  let sec = 0;
+  function pad ( val ) { return val > 9 ? val : "0" + val; }
+  setInterval( function(){
+      if (gameEndCount === 8) {
+        return;
+      }
+      document.getElementById("seconds").innerHTML=pad(++sec%60);
+      document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+  }, 1000);
+
+}
+
+function gameEnd(){
+  gameResult();
+}
+
 function gameResult() {
   alert('Congratus your Win!!' );
   alert(gameMoves);
 }
 
+document.querySelector('.restart-game').addEventListener('click', function (evt) {
+
+});
+
 
 document.querySelector('.main-playground-container').addEventListener('click', function (evt) {
      if($(evt.target).attr('class') === 'playground-cell-front' && roundMoves !== 2) {
-      clickedCell(evt);
+       clickedCell(evt);
     }
 });
 
