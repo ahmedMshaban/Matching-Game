c440d46c117a1193c5639e64f9352ef25f39034d
ahmedMshaban <ahmed.ms.elias@gmail.com>
2018-03-23 11:17:17 +0300

    Write documentation about the game and add code comments

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Matching-Game
+
+## Table of Contents
+
+* [How The Game Works](#HowGameWorks)
+* [Game Functionality](#Functionality)
+
+## How The Game Works
+
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!
+
+Each turn:
+
+  -The player flips one card over to reveal its underlying symbol.
+  -The player then turns over a second card, trying to find the corresponding card with the same symbol.
+  -If the cards match, both cards stay flipped over.
+  -If the cards do not match, both cards are flipped face down.
+The game ends once all cards have been correctly matched.
+
+## Game Functionality
+
+### Correct Guess
+If the cards match, both cards stay flipped over and the background color will change.
+
+### Incorrect Guess
+If the cards do not match, both cards are flipped face down.
+
+### Moves
+Each two guesses(correct or incorrect) the number of moves will increase by one.
+
+### Time
+When the player start to click on the card and make first guess the time tracker will start.
+
+### Stars
+-When the game start the player will have 3 stars with (orange color) after 17 guesses (17 clicks on the cards) the stars will decrease by 1 so the user will have 2 stars with orange color only.
+-After after another 8 guesses (25 guess on the total) the stars will decrease by 1 so the user will have 1 star with orange color only.
+-After after another 8 guesses (33 guess on the total) the stars will decrease by 1 so the user will have 0 star.
+
+### Restart
+When the player click on the restart button the game will start over and will change the cards places.
--- js/app.js
+++ js/app.js
@@ -1,144 +1,149 @@
+//declare the main variables and array
 let roundMoves = 0;
 let gameMoves = 0;
 let gameEndCount = 0;
 let clicksCounts = 0;
-const symbols = ['images/boat.png', 'images/boat.png', 'images/building.png', 'images/building.png', 'images/daruma.png', 'images/daruma.png', 'images/doll.png', 'images/doll.png', 'images/star.png', 'images/star.png', 'images/teddy-bear.png', 'images/teddy-bear.png', 'images/toys.png', 'images/toys.png', 'images/tricycle.png', 'images/tricycle.png']
+let symbols = ['images/boat.png', 'images/boat.png', 'images/building.png', 'images/building.png', 'images/daruma.png', 'images/daruma.png', 'images/doll.png', 'images/doll.png', 'images/star.png', 'images/star.png', 'images/teddy-bear.png', 'images/teddy-bear.png', 'images/toys.png', 'images/toys.png', 'images/tricycle.png', 'images/tricycle.png']
 let openCell = [];
 let timeExcuation;
 
 
 function clickedCell(evt) {
-    if (gameMoves === 0 && roundMoves === 0) {
-        timeCalculator();
-    }
+	//Start the game time tracker
+	if (gameMoves === 0 && roundMoves === 0) {
+		timeCalculator();
+	}
 
-    clicksCounts++;
-    switch (clicksCounts) {
-        case 17:
-            $('.star-rating span:nth-child(3)').removeClass('checked');
-            break;
-        case 25:
-            $('.star-rating span:nth-child(2)').removeClass('checked');
-            break;
-        case 33:
-            $('.star-rating span:nth-child(1)').removeClass('checked');
-            break;
-    }
+	//Track the clicks so I can use it on the stars rating system
+	clicksCounts++;
 
-    if (roundMoves === 0) {
-        roundMoves++;
-        $(evt.target).addClass('clicked');
-        $(evt.target).toggleClass('flipped');
-        openCell[0] = $(evt.target).children('img').attr('src');
-    } else if (roundMoves === 1) {
-        //Prevent more than 2 click on the same round
-        roundMoves = 2;
-        $(evt.target).addClass('clicked');
-        $(evt.target).toggleClass('flipped');
-        openCell[1] = $(evt.target).children('img').attr('src');
-        gameMoves++;
-        //Update game moves
-        $('.game-moves').text(gameMoves);
-        window.setTimeout(roundCheck, 1000);
-    }
+	switch (clicksCounts) {
+		case 17:
+			$('.star-rating span:nth-child(3)').removeClass('checked');
+			break;
+		case 25:
+			$('.star-rating span:nth-child(2)').removeClass('checked');
+			break;
+		case 33:
+			$('.star-rating span:nth-child(1)').removeClass('checked');
+			break;
+	}
+
+	if (roundMoves === 0) {
+		roundMoves++;
+		$(evt.target).addClass('clicked');
+		$(evt.target).toggleClass('flipped');
+		openCell[0] = $(evt.target).children('img').attr('src');
+	} else if (roundMoves === 1) {
+		//Prevent more than 2 click on the same round
+		roundMoves = 2;
+		$(evt.target).addClass('clicked');
+		$(evt.target).toggleClass('flipped');
+		openCell[1] = $(evt.target).children('img').attr('src');
+		gameMoves++;
+		//Update game moves
+		$('.game-moves').text(gameMoves);
+		window.setTimeout(roundCheck, 1000);
+	}
 }
 
 function roundCheck() {
-    if (openCell[0] === openCell[1]) {
-        $('.clicked').addClass('identical pulse');
-        gameEndCount++;
-        if (gameEndCount === 8) {
-            gameResult();
-        }
-    }
-    $('.main-playground-container .playground-cell').removeClass('flipped clicked');
-    //Reset the round
-    roundMoves = 0;
-    //Empty the aray
-    openCell = [];
+	if (openCell[0] === openCell[1]) {
+		$('.clicked').addClass('identical pulse');
+		gameEndCount++;
+		if (gameEndCount === 8) {
+			gameResult();
+		}
+	}
+	$('.main-playground-container .playground-cell').removeClass('flipped clicked');
+	//Reset the round
+	roundMoves = 0;
+	//Empty the aray
+	openCell = [];
 }
 
 function shuffle(array) {
-    let currentIndex = array.length,
-        temporaryValue, randomIndex;
+	let currentIndex = array.length,
+		temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 function distributionSymbols(array) {
-    shuffle(symbols);
-    for (let i = 0; i <= array.length; i++) {
-        $('.playground-cell:nth-child(' + (i + 1) + ') img').attr('src', array[i]);
-    }
+	shuffle(symbols);
+	for (let i = 0; i <= array.length; i++) {
+		$('.playground-cell:nth-child(' + (i + 1) + ') img').attr('src', array[i]);
+	}
 }
 
 function timeCalculator() {
-    let sec = 0;
+	let sec = 0;
 
-    function pad(val) {
-        return val > 9 ? val : '0' + val;
-    }
-    timeExcuation = setInterval(function() {
-        if (gameEndCount === 8) {
-            return;
-        }
-        document.getElementById('seconds').innerHTML = pad(++sec % 60);
-        document.getElementById('minutes').innerHTML = pad(parseInt(sec / 60, 10));
-    }, 1000);
-
+	function pad(val) {
+		return val > 9 ? val : '0' + val;
+	}
+	timeExcuation = setInterval(function () {
+		//To stop the game time tracker
+		if (gameEndCount === 8) {
+			return;
+		}
+		document.getElementById('seconds').innerHTML = pad(++sec % 60);
+		document.getElementById('minutes').innerHTML = pad(parseInt(sec / 60, 10));
+	}, 1000);
 }
 
 function gameResult() {
-    $('#resultModal').css('display', 'block');
-    $('.resault-moves').text('Your Moves: ' + gameMoves);
-    $('.resault-time').text('Your time: ' + document.getElementById('minutes').innerHTML + " : " + document.getElementById('seconds').innerHTML);
+	$('#resultModal').css('display', 'block');
+	$('.resault-moves').text('Your Moves: ' + gameMoves);
+	$('.resault-time').text('Your time: ' + document.getElementById('minutes').innerHTML + " : " + document.getElementById('seconds').innerHTML);
 }
 
-
-// When the user clicks on <span> (x), close the modal
-document.querySelector('.close').addEventListener('click', function() {
-    $('#resultModal').css('display', 'none');
+// When the user clicks on (x), close the modal
+document.querySelector('.close').addEventListener('click', function () {
+	$('#resultModal').css('display', 'none');
 });
-
 
 // When the user clicks anywhere outside of the modal, close it
-$(window).click(function(evt) {
-    if (evt.target == $('#resultModal')[0]) {
-        $('#resultModal').css('display', 'none');
-    }
+$(window).click(function (evt) {
+	if (evt.target == $('#resultModal')[0]) {
+		$('#resultModal').css('display', 'none');
+	}
 });
 
-$(".restart-game").each(function() {
-    let restartGame = this;
-    restartGame.addEventListener("click", function(evt) {
-        gameMoves = 0;
-        roundMoves = 0;
-        clicksCounts = 0;
-        $('.game-moves').text(gameMoves);
-        gameEndCount = 0;
-        openCell = [];
-        $('.main-playground-container .playground-cell').removeClass('flipped clicked identical pulse');
-        $('.star-rating span').addClass('checked');
-        clearInterval(timeExcuation);
-        $('#seconds').text('00');
-        $('#minutes').text('00');
-        window.setTimeout(distributionSymbols, 1000, symbols);
-        $('#resultModal').css('display', 'none');
-    });
+$(".restart-game").each(function () {
+	let restartGame = this;
+	restartGame.addEventListener("click", function (evt) {
+		gameMoves = 0;
+		roundMoves = 0;
+		clicksCounts = 0;
+		$('.game-moves').text(gameMoves);
+		gameEndCount = 0;
+		openCell = [];
+		$('.main-playground-container .playground-cell').removeClass('flipped clicked identical pulse');
+		$('.star-rating span').addClass('checked');
+		clearInterval(timeExcuation);
+		$('#seconds').text('00');
+		$('#minutes').text('00');
+		window.setTimeout(distributionSymbols, 1000, symbols);
+		$('#resultModal').css('display', 'none');
+	});
 });
 
-document.querySelector('.main-playground-container').addEventListener('click', function(evt) {
-    if ($(evt.target).attr('class') === 'playground-cell' && roundMoves !== 2) {
-        clickedCell(evt);
-    }
+//The main Event Listener for the playground
+document.querySelector('.main-playground-container').addEventListener('click', function (evt) {
+	//Check if the user click on a card and disable click when there is two cards open.
+	if ($(evt.target).attr('class') === 'playground-cell' && roundMoves !== 2) {
+		clickedCell(evt);
+	}
 });
 
+//Re arrange the array that's hold the card images
 distributionSymbols(symbols);
